ee2c4cffa1052bc7bb36b1a20b8a85db544efdc4
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2024-10-19 10:05:44 +0200

    Modification de la vue de recheerche (Deseign et fonctionnalitées)

---
+++ Reciplease/View/UtilsViews/RecommendationView.swift
@@ -0,0 +1,18 @@
+//
+//  RecommendationView.swift
+//  Reciplease
+//
+//  Created by Sebby on 15/10/2024.
+//
+
+import SwiftUI
+
+struct RecommendationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecommendationView()
+}
---
+++ Reciplease/View/UtilsViews/SearchRecipeTextFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchRecipeTextFieldView.swift
+//  Reciplease
+//
+//  Created by Sebby on 15/10/2024.
+//
+
+import SwiftUI
+
+struct SearchRecipeTextFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchRecipeTextFieldView()
+}
